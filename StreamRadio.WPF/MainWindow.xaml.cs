1a15fe887f22d6533d3fa27197bd96f7f19c549b
Ronald <ronaldme@gmail.com>
2020-01-04 08:44:17 +0100

    Add NAudio package / sample code

--- StreamRadio.WPF/MainWindow.xaml.cs
+++ StreamRadio.WPF/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using NAudio.Wave;
 
 namespace StreamRadio.WPF
 {
@@ -23,6 +13,25 @@ namespace StreamRadio.WPF
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            new Task(() =>
+            {
+                var url = "http://media.ch9.ms/ch9/2876/fd36ef30-cfd2-4558-8412-3cf7a0852876/AzureWebJobs103.mp3";
+                using (var mf = new MediaFoundationReader(url))
+                using (var wo = new WaveOutEvent())
+                {
+                    wo.Init(mf);
+                    wo.Play();
+                    while (wo.PlaybackState == PlaybackState.Playing)
+                    {
+                        Thread.Sleep(1000);
+                    }
+                }
+            }).Start();
+            
         }
     }
 }
