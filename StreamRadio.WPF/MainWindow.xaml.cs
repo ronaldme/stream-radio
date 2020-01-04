69e849af0353a90130ea34a7fce4440a63a0c26f
Ronald <ronaldme@gmail.com>
2020-01-04 16:50:37 +0100

    Add music stations

--- StreamRadio.WPF/MainWindow.xaml.cs
+++ StreamRadio.WPF/MainWindow.xaml.cs
@@ -1,37 +1,110 @@
-﻿using System.Threading;
+﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using NAudio.Wave;
 
 namespace StreamRadio.WPF
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private CancellationTokenSource _source;
+
+        private Task _task;
+
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void QMusicFouteUurClick(object sender, RoutedEventArgs e)
         {
-            new Task(() =>
+            string url = "https://icecast-qmusicnl-cdp.triple-it.nl/Qmusic_nl_fouteuur_96.mp3";
+            HandleStreamClick(url);
+        }
+
+        private void QMusicClick(object sender, RoutedEventArgs e)
+        {
+            string url = "https://icecast-qmusicnl-cdp.triple-it.nl/Qmusic_nl_live_96.mp3";
+            HandleStreamClick(url);
+        }
+
+        private void Radio10Click(object sender, RoutedEventArgs e)
+        {
+            string url = "http://icecast-qmusicnl-cdp.triple-it.nl/Qmusic_nl_live_96.mp3";
+            HandleStreamClick(url);
+        }
+
+        private void VeronicaClick(object sender, RoutedEventArgs e)
+        {
+            string url = "https://20873.live.streamtheworld.com/VERONICA.mp3";
+            HandleStreamClick(url);
+        }
+
+        private void SkyRadioClick(object sender, RoutedEventArgs e)
+        {
+            string url = "https://19993.live.streamtheworld.com/SKYRADIO.mp3";
+            HandleStreamClick(url);
+        }
+
+        private void BNRClick(object sender, RoutedEventArgs e)
+        {
+            string url = "http://icecast-bnr.cdp.triple-it.nl/bnr_mp3_96_04.m3u";
+            HandleStreamClick(url);
+        }
+
+        private void SlamClick(object sender, RoutedEventArgs e)
+        {
+            string url = "https://stream.slam.nl/slam_mp3";
+            HandleStreamClick(url);
+        }
+
+        private void Radio1Click(object sender, RoutedEventArgs e)
+        {
+            string url = "http://icecast.omroep.nl/radio1-bb-mp3";
+            HandleStreamClick(url);
+        }
+
+        private void Radio2Click(object sender, RoutedEventArgs e)
+        {
+            string url = "http://icecast.omroep.nl/radio2-bb-mp3";
+            HandleStreamClick(url);
+        }
+
+        private void ArrowClassicRockClick(object sender, RoutedEventArgs e)
+        {
+            string url = "https://stream.gal.io/arrow";
+            HandleStreamClick(url);
+        }
+
+        private void HandleStreamClick(string url)
+        {
+            _source?.Cancel();
+
+            var source = new CancellationTokenSource();
+            _task = CreateNewTask(url, source.Token);
+            _task.Start();
+        }
+
+        private Task CreateNewTask(string url, CancellationToken sourceToken)
+        {
+            return new Task(() =>
             {
-                var url = "http://media.ch9.ms/ch9/2876/fd36ef30-cfd2-4558-8412-3cf7a0852876/AzureWebJobs103.mp3";
                 using (var mf = new MediaFoundationReader(url))
                 using (var wo = new WaveOutEvent())
                 {
                     wo.Init(mf);
                     wo.Play();
+
                     while (wo.PlaybackState == PlaybackState.Playing)
                     {
-                        Thread.Sleep(1000);
+                        Task.Delay(1000, sourceToken);
+
+                        if (sourceToken.IsCancellationRequested)
+                            throw new TaskCanceledException();
                     }
                 }
-            }).Start();
-            
+            }, sourceToken);
         }
     }
 }
