3d2d22512418e4acf7138c11ce59840c1fcae212
Ronald <ronaldme@gmail.com>
2020-01-05 10:39:11 +0100

    Add User / UserRepository

---
+++ StreamRadio.DAL/User.cs
@@ -0,0 +1,9 @@
+﻿namespace StreamRadio.DAL
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public long ChatId { get; set; }
+    }
+}
---
+++ StreamRadio.DAL/UserRepository.cs
@@ -0,0 +1,23 @@
+﻿using StreamRadio.DAL.Infrastructure;
+
+namespace StreamRadio.DAL
+{
+    public class UserRepository : RepositoryBase<User>
+    {
+        public User GetUser(long chatId)
+        {
+            using var db = Create();
+            return Collection(db).FindOne(c => c.ChatId == chatId);
+        }
+
+        public void CreateUser(string name, long chatId)
+        {
+            using var db = Create();
+            Collection(db).Insert(new User
+            {
+                Name = name,
+                ChatId = chatId,
+            });
+        }
+    }
+}
