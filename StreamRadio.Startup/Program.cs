8a599b41d853808723df6d71aac4408c9f007d13
Ronald <ronaldme@gmail.com>
2020-01-05 09:45:09 +0100

    Add StreamRadio.Startup

---
+++ StreamRadio.Startup/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using Topshelf;
+
+namespace StreamRadio.Startup
+{
+    public static class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            HostFactory.Run(hostConfig =>
+            {
+                hostConfig.StartAutomaticallyDelayed();
+
+                hostConfig.EnableServiceRecovery(serviceRecovery =>
+                {
+                    serviceRecovery.RestartService(5);
+                });
+
+                hostConfig.Service<Service>(serviceConfig =>
+                {
+                    serviceConfig.ConstructUsing(() => new Service());
+                    serviceConfig.WhenStarted(s => s.Start());
+                    serviceConfig.WhenStopped(s => s.Stop());
+                });
+            });
+        }
+    }
+}
---
+++ StreamRadio.Startup/Service.cs
@@ -0,0 +1,15 @@
+﻿using StreamRadio.Services;
+
+namespace StreamRadio.Startup
+{
+    public class Service
+    {
+        public void Start()
+        {
+        }
+
+        public void Stop()
+        {
+        }
+    }
+}
