088965b886e8eebd8fa7986bf20fac43b818aaa3
Ronald <ronaldme@gmail.com>
2020-01-05 12:04:21 +0100

    Add PostHelper

---
+++ StreamRadio.Startup/Helpers/PostHelper.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using System.Net;
+using Newtonsoft.Json.Linq;
+
+namespace StreamRadio.Startup.Helpers
+{
+    public class PostHelper
+    {
+        public static void Post(string postUrl, string text)
+        {
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create(postUrl);
+            httpWebRequest.ContentType = "application/json";
+            httpWebRequest.Method = "POST";
+
+            using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+            {
+                var json = JObject.FromObject(new { Text = text });
+                streamWriter.Write(json);
+            }
+
+            var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+            using var streamReader = new StreamReader(httpResponse.GetResponseStream());
+            streamReader.ReadToEnd();
+        }
+    }
+}
