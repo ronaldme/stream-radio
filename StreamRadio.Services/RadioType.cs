30128e78eb9b141fcbcd3665e7c98c14387409ab
Ronald <ronaldme@gmail.com>
2020-01-05 12:03:44 +0100

    Add Descriptions to RadioType

--- StreamRadio.Services/RadioType.cs
+++ StreamRadio.Services/RadioType.cs
@@ -1,17 +1,39 @@
-﻿namespace StreamRadio.Services
+﻿using System.ComponentModel;
+
+namespace StreamRadio.Services
 {
     public enum RadioType
     {
         None,
+
+        [Description("Q-music foute uur")]
         QMusicFouteUur,
+
+        [Description("Q-music")]
         QMusic,
+
+        [Description("Radio 10")]
         Radio10,
+
+        [Description("Veronica")]
         Veronica,
+
+        [Description("SkyRadio")]
         SkyRadio,
+
+        [Description("BNR")]
         BNR,
+
+        [Description("SLAM!")]
         Slam,
+
+        [Description("Radio 1")]
         Radio1,
+
+        [Description("Radio 2")]
         Radio2,
+
+        [Description("Arrow Classic Rock")]
         ArrowClassicRock,
     }
 }
