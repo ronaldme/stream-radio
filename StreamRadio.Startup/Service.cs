5f18eb7d4f0e07d523fb605582da76e34aeafa09
Ronald <ronaldme@gmail.com>
2020-01-05 10:23:10 +0100

    Add initial telegram bot code

--- StreamRadio.Startup/Service.cs
+++ StreamRadio.Startup/Service.cs
@@ -1,15 +1,17 @@
-﻿using StreamRadio.Services;
-
-namespace StreamRadio.Startup
+﻿namespace StreamRadio.Startup
 {
     public class Service
     {
+        private readonly TelegramService _telegramService = new TelegramService();
+
         public void Start()
         {
+            _telegramService.Start();
         }
 
         public void Stop()
         {
+            _telegramService.Stop();
         }
     }
 }
---
+++ StreamRadio.Startup/TelegramService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using log4net;
+using System.Configuration;
+using System.Threading.Tasks;
+using StreamRadio.Services;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+namespace StreamRadio.Startup
+{
+    public class TelegramService
+    {
+        private static readonly ILog Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        private readonly Radio _radio = new Radio();
+        private readonly TelegramBotClient _bot = new TelegramBotClient(ConfigurationManager.AppSettings["telegramToken"]);
+
+        public void Start()
+        {
+            _bot.OnMessage += BotOnMessageReceived;
+            _bot.StartReceiving(Array.Empty<UpdateType>());
+
+            Log.Info($"{nameof(TelegramService)} Started");
+        }
+
+        private async void BotOnMessageReceived(object sender, MessageEventArgs e)
+        {
+            var message = e.Message;
+            if (message == null || message.Type != MessageType.Text) return;
+
+            try
+            {
+                Log.Info($"Received: {e.Message.Text}");
+            }
+            catch (Exception exception)
+            {
+                Log.Error($"Could not handle message {message.Text}", exception);
+            }
+        }
+
+        public void Stop() => Log.Info($"{nameof(TelegramService)} stopped");
+    }
+}
